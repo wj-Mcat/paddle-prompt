853fe9c71187a0ef37ebf524f38a9dc275378ece
wj-Mcat <1435130236@qq.com>
2022-03-09 13:21:25 +0800

    init setup installation script

---
+++ setup.py
@@ -0,0 +1,86 @@
+"""
+setup
+"""
+import os
+from typing import List
+
+import semver
+import setuptools
+
+
+def versioning(version: str) -> str:
+    """
+    version to specification
+    Author: wj-Mcat <wjmcater@gmail.com> (https://github.com/wj-Mcat)
+    """
+    sem_ver = semver.parse(version)
+
+    major = sem_ver['major']
+    minor = sem_ver['minor']
+    patch = str(sem_ver['patch'])
+
+    if minor % 2:
+        patch = 'dev' + patch
+
+    fin_ver = '%d.%d.%s' % (
+        major,
+        minor,
+        patch,
+    )
+
+    return fin_ver
+
+
+def get_version() -> str:
+    """
+    read version from VERSION file
+    """
+    version = '0.0.0'
+
+    with open(
+            os.path.join(
+                os.path.dirname(__file__),
+                'VERSION'
+            )
+    ) as version_fh:
+        # 1. Get X.Y.Z
+        version = version_fh.read().strip()
+        # 2. versioning from X.Y.Z to X.Y.devZ
+        version = versioning(version)
+
+    return version
+
+
+def get_long_description() -> str:
+    """get long_description"""
+    with open('README.md', 'r') as readme_fh:
+        return readme_fh.read()
+
+
+def get_install_requires() -> List[str]:
+    """get install_requires"""
+    with open('requirements.txt', 'r') as requirements_fh:
+        return requirements_fh.read().splitlines()
+
+
+setuptools.setup(
+    name='paddle-prompt',
+    version=get_version(),
+    author='wj-Mcat (吴京京)',
+    author_email='wjmcater@gmail.com',
+    description='Prompt Toolkit for PaddlePaddle Deep Learning Framework',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    license='Apache-2.0',
+    url='https://github.com/wj-Mcat/paddle-prompt',
+    packages=setuptools.find_packages('paddle_prompt'),
+    package_dir={'': ''},
+    install_requires=get_install_requires(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
